e656a80f22b946158a594e4619fc4f48191962c9
eden melaku <edenzzzmelaku@gmail.com>
2020-03-09 18:15:21 +0300

    implementing abbrivations lookup and add

--- Data-preprocessor/Abbrivations.py
+++ Data-preprocessor/Abbrivations.py
@@ -1,6 +1,27 @@
+import csv
+
+
 
 class Abbrivations:
 
     def __init__(self, abb):
         self.abb=abb
+
+
     def lookup(self):
+        try:
+            with open("abbrivations.csv", "a", newline="\n")as csv_file:
+                    line_reader = csv.reader(csv_file)
+                    r1 = csv.get_dialect(self.abb)
+                    return r1
+        finally:
+            csv_file.close()
+
+    def add_abbrivation(self, abb, word):
+        with open("abbrivations.csv", "a", newline="\n")as csv_file:
+            line_writer = csv.writer(csv_file)
+            line_writer.writerow([abb,word])
+
+
+
+
