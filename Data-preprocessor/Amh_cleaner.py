e8d2a435c7fbd066b748028979a35c3f8c776c69
eden melaku <edenzzzmelaku@gmail.com>
2020-03-10 14:41:15 +0300

    some errors are fixed

--- Data-preprocessor/Amh_cleaner.py
+++ Data-preprocessor/Amh_cleaner.py
@@ -1,23 +1,31 @@
 from re import split, match
 from .AmahricNumber import  Amharic_Numbers
+from .Abbrivations import Abbrivations
 class Cleaner:
     def __init__(self,text):
         self.text=text
 
-    def clean_numbers(self):
+def clean(self):
 
         words=split(" ",self.text)
         text=""
         i=0
         for word in words:
-            if(match([0-9],word)):
+            if match([0 - 9], word):
                 number=Amharic_Numbers(word)
                 if(words[i+1] == "q.m" || words[i+1] == "q/m" ):
                     text=text+" "+number.convert_year()
                 else:
                     text=text+" "+number.num_to_word()
 
-            elif(word.__contains__("/") || word.__contains__(".")):
+            elif word.__contains__("/") or word.__contains__("."):
+
+                abrivaiton=Abbrivations(word)
+                text=text+ " "+abrivaiton.lookup()
+
+
+
+
 
 
 
