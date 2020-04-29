ac53433e67976ebdf187fc25d4f06712c43b538e
eden melaku <edenzzzmelaku@gmail.com>
2020-04-29 12:49:50 +0300

    location layer is changed

--- models/Attention.py
+++ models/Attention.py
@@ -18,21 +18,24 @@ class Linear(nn.Module):
 
 
 class Location_layer(nn.Module):
-    def __init__(self, n_filters, kernel_size, dim):
+    def __init__(self, attention_n_filters, attention_kernel_size,
+                 attention_dim):
         super(Location_layer, self).__init__()
-        padding = int((kernel_size-1) / 2)
-        self.location_conv = Conv(2, n_filters,
-                                      kernel_size=kernel_size,
+        padding = int((attention_kernel_size - 1) / 2)
+        self.location_conv = Conv(2, attention_n_filters,
+                                      kernel_size=attention_kernel_size,
                                       padding=padding, bias=False, stride=1,
                                       dilation=1)
-        self.location_dense = Linear(n_filters, dim, bias=False,
-                                     w_init_gain='tanh')
+        self.location_dense = Linear(attention_n_filters, attention_dim,
+                                         bias=False, w_init_gain='tanh')
 
     def forward(self, attention_weights_cat):
-        output = self.location_conv(attention_weights_cat)
-        output = output.transpose(1, 2)
-        output = self.location_dense(output)
-        return output
+        processed_attention = self.location_conv(attention_weights_cat)
+        processed_attention = processed_attention.transpose(1, 2)
+        processed_attention = self.location_dense(processed_attention)
+        return processed_attention
+
+
 '''
 We use the location-sensitive attention
 from [21], which extends the additive attention mechanism [22] to
